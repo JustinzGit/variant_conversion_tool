3880a8a445a8203684601f8d0f11396caa28b2e9
Justin <justinms@outlook.com>
2020-08-23 15:46:04 -0500

    remove quotes from collected data

--- conversion/models/ensemble.py
+++ conversion/models/ensemble.py
@@ -55,7 +55,7 @@ class Ensemble(models.Model):
         return {
             "chromosome": repr(response["mappings"][0]["seq_region_name"]).replace("'",""),
             "gdna_location": repr(response["mappings"][0]["start"]),
-            "assembly": repr(response["mappings"][0]["assembly_name"]),
+            "assembly": repr(response["mappings"][0]["assembly_name"]).strip("'"),
             "strand": repr(response["mappings"][0]["strand"])
         }
 
