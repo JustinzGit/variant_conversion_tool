0c1099f4d2e70a3f66f343235c0db0f3c13d869b
Justin <justinms@outlook.com>
2020-08-22 19:58:44 -0500

    add gene attributes and sequence format method

--- conversion/models.py
+++ conversion/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 class Gene(models.Model):
-    def __init__(self, name, sequence):
-        self.name = name
-        self.sequence = sequence
+    name = models.CharField(max_length=8)
+    sequence = models.TextField()
         
+    def format_sequence(self):
+        cdna_seq = self.sequence.read().decode("utf-8").replace("\n", "").replace("\r", "").upper()
+
+        # Store sequence in a list of single characters
+        nt_seq = list(cdna_seq)
+
+        # Group nucleotides by 3
+        codon_seq = []
+        if len(nt_seq) % 3 == 0:
+            for i in range(0, len(nt_seq), 3):
+                codon = list(nt_seq[i:i + 3])
+                codon_seq.append(codon)
+                
+        return codon_seq
