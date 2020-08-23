ec060af97702a229c8c3749c58687cca091f4cdf
Justin <justinms@outlook.com>
2020-08-23 11:51:45 -0500

    render amino acid table at index

--- conversion/views.py
+++ conversion/views.py
@@ -2,7 +2,33 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-    return render(request, "conversion/index.html")
+
+    aa_table = [
+    ['M', 'Methionine', 'Met', 'Non-Polar, Sulfur-Containing', 'Essential'],
+    ['C', 'Cysteine', 'Cys', 'Non-Polar, Sulfur-Containing', 'Non-Essential'],
+    ['W', 'Tryptophan', 'Trp', 'Non-Polar, Aromatic', 'Essential'],
+    ['F', 'Phenylalanine', 'Phe', 'Non-Polar, Aromatic', 'Essential'],
+    ['I', 'Isoleucine', 'Ile', 'Non-Polar, Aliphatic', 'Essential'],
+    ['G', 'Glycine', 'Gly', 'Non-Polar, Aliphatic', 'Non-Essential'],
+    ['V', 'Valine', 'Val', 'Non-Polar, Aliphatic', 'Essential'],
+    ['A', 'Alanine', 'Ala', 'Non-Polar, Aliphatic', 'Non-Essential'],
+    ['P', 'Proline', 'Pro', 'Non-Polar, Aliphatic', 'Non-Essential'],
+    ['L', 'Leucine', 'Leu', 'Non-Polar, Aliphatic', 'Essential'],
+    ['N', 'Asparagine', 'Asn', 'Polar, Amidic', 'Non-Essential'],
+    ['Q', 'Glutamine', 'Gln', 'Polar, Amidic', 'Non-Essential'],
+    ['Y', 'Tyrosine', 'Tyr', 'Polar, Aromatic', 'Non-Essential'],
+    ['T', 'Threonine', 'Thr', 'Polar, Hydroxylic', 'Essential'],
+    ['S', 'Serine', 'Ser', 'Polar, Hydroxylic', 'Non-Essential'],
+    ['K', 'Lysine', 'Lys', 'Positively-Charged, Basic', 'Essential'],
+    ['H', 'Histidine', 'His', 'Positively-Charged, Basic', 'Essential'],
+    ['R', 'Arginine', 'Arg', 'Positively-Charged, Basic', 'Non-Essential'],
+    ['D', 'Aspartic Acid', 'Asp', 'Negatively-Charged, Acidic', 'Non-Essential'],
+    ['E', 'Glutamic Acid', 'Glu', 'Negatively-Charged, Acidic', 'Non-Essential']
+    ]
+
+    return render(request, "conversion/index.html", {
+        "aa_table": aa_table
+    })
 
 def conversion(request):
     if request.method == "POST":
@@ -19,7 +45,6 @@ def conversion(request):
             return render(request, "conversion/index.html", {
                 "alert": "A cDNA submission is required",
             })
-    
         gene = Gene(
             name = request.POST["gene_name"],
             cdna_seq = cdna_seq
