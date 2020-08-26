915a46b86156238fd6d4536828e900ddf020e3a3
Justin <justinms@outlook.com>
2020-08-25 23:27:33 -0500

    move amino acid table into class variable

--- conversion/views.py
+++ conversion/views.py
@@ -2,32 +2,8 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-
-    aa_table = [
-    ['M', 'Methionine', 'Met', 'Non-Polar, Sulfur-Containing', 'Essential'],
-    ['C', 'Cysteine', 'Cys', 'Non-Polar, Sulfur-Containing', 'Non-Essential'],
-    ['W', 'Tryptophan', 'Trp', 'Non-Polar, Aromatic', 'Essential'],
-    ['F', 'Phenylalanine', 'Phe', 'Non-Polar, Aromatic', 'Essential'],
-    ['I', 'Isoleucine', 'Ile', 'Non-Polar, Aliphatic', 'Essential'],
-    ['G', 'Glycine', 'Gly', 'Non-Polar, Aliphatic', 'Non-Essential'],
-    ['V', 'Valine', 'Val', 'Non-Polar, Aliphatic', 'Essential'],
-    ['A', 'Alanine', 'Ala', 'Non-Polar, Aliphatic', 'Non-Essential'],
-    ['P', 'Proline', 'Pro', 'Non-Polar, Aliphatic', 'Non-Essential'],
-    ['L', 'Leucine', 'Leu', 'Non-Polar, Aliphatic', 'Essential'],
-    ['N', 'Asparagine', 'Asn', 'Polar, Amidic', 'Non-Essential'],
-    ['Q', 'Glutamine', 'Gln', 'Polar, Amidic', 'Non-Essential'],
-    ['Y', 'Tyrosine', 'Tyr', 'Polar, Aromatic', 'Non-Essential'],
-    ['T', 'Threonine', 'Thr', 'Polar, Hydroxylic', 'Essential'],
-    ['S', 'Serine', 'Ser', 'Polar, Hydroxylic', 'Non-Essential'],
-    ['K', 'Lysine', 'Lys', 'Positively-Charged, Basic', 'Essential'],
-    ['H', 'Histidine', 'His', 'Positively-Charged, Basic', 'Essential'],
-    ['R', 'Arginine', 'Arg', 'Positively-Charged, Basic', 'Non-Essential'],
-    ['D', 'Aspartic Acid', 'Asp', 'Negatively-Charged, Acidic', 'Non-Essential'],
-    ['E', 'Glutamic Acid', 'Glu', 'Negatively-Charged, Acidic', 'Non-Essential']
-    ]
-
     return render(request, "conversion/index.html", {
-        "aa_table": aa_table
+        "aa_table": Gene.aa_info
     })
 
 # Variant to test on
